419813479cbe56eb65ce8ec3cd879826aa2a6810
V1v3kShah <94179705+Vivek-C-Shah@users.noreply.github.com>
2024-03-19 00:19:04 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+export default {
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: './',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
+}
